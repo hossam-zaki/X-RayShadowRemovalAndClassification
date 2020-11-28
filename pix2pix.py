5d699da388a7662858297a8951306b32c9ce9990
nahmed12 <nasheath_ahmed@brown.edu>
2020-11-28 01:25:02 +0000

    tried lab autoencoder and decoder

--- pix2pix.py
+++ pix2pix.py
@@ -35,11 +35,15 @@ class Pix2Pix():
         patch = int(self.img_rows / 2**4)
         self.disc_patch = (patch, patch, 1)
         # Number of filters in the first layer of G and D
-        self.gf = 16
-        self.df = 16
-        optimizer = Adam(0.01, 0.5)
+        self.gf = 32
+        self.df = 32
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+        optimizer = Adam(0.001, 0.5)
         # Build and compile the discriminator
         self.discriminator = self.build_discriminator()
+        #Loading in the weights here NEED TO DELETE AFTER THE RUN WE DO  TODAY
+        #self.discriminator.load_weights("discriminatorWeights.h5")
         self.discriminator.compile(loss='mse',
             optimizer=optimizer,
             metrics=['accuracy'])
@@ -55,56 +59,32 @@ class Pix2Pix():
         img_B = Input(shape=self.img_shape)
         # By conditioning on B generate a fake version of A
         fake_A = self.generator(img_B)
+        #Loading in the weights here NEED TO DELETE AFTER THE RUN WE DO  TODAY
+        #self.generator.load_weights("generatorWeights.h5")
         # For the combined model we will only train the generator
         self.discriminator.trainable = False
         # Discriminators determines validity of translated images / condition pairs
         valid = self.discriminator([fake_A, img_B])
+
         self.combined = Model(inputs=[img_A, img_B], outputs=[valid, fake_A])
+
+        #Loading in the weights here NEED TO DELETE AFTER THE RUN WE DO  TODAY
+        #self.combined.load_weights("ganWeights.h5")
         self.combined.compile(loss=['mse', 'mae'],
                               loss_weights=[1, 100],
                               optimizer=optimizer)
+
     def build_generator(self):
         """U-Net Generator"""
-        def conv2d(layer_input, filters, f_size=5, bn=True):
-            """Layers used during downsampling"""
-            d = Conv2D(filters, kernel_size=f_size, strides=2, padding='same')(layer_input)
-            d = LeakyReLU(alpha=0.2)(d)
-            if bn:
-                d = BatchNormalization(momentum=0.8)(d)
-            return d
-        def deconv2d(layer_input, skip_input, filters, f_size=5, dropout_rate=0):
-            """Layers used during upsampling"""
-            u = Conv2DTranspose(filters, kernel_size=f_size, padding='SAME', activation='relu', strides=2, 
-            kernel_initializer=tf.keras.initializers.RandomNormal(stddev=0.1))(layer_input)
-            # u = UpSampling2D(size=2)(layer_input)
-            # u = Conv2D(filters, kernel_size=f_size, strides=1, padding='same', activation='relu')(u)
-            if dropout_rate:
-                u = Dropout(dropout_rate)(u)
-            u = BatchNormalization(momentum=0.8)(u)
-            #u = Concatenate()([u, skip_input])
-            return u
-        # Image input
-        d0 = Input(shape=self.img_shape)
-        # Downsampling
-        d1 = conv2d(d0, self.gf, bn=False)
-        d2 = conv2d(d1, self.gf*2) #32
-        d3 = conv2d(d2, self.gf*4) #64
-        #d4 = conv2d(d3, self.gf*8) #128 
-        # d5 = conv2d(d4, self.gf*8)
-        # d6 = conv2d(d5, self.gf*8)
-        # d7 = conv2d(d6, self.gf*8)
-        # Upsampling
-        # u1 = deconv2d(d7, d6, self.gf*8)
-        # u2 = deconv2d(u1, d5, self.gf*8)
-        # u3 = deconv2d(u2, d4, self.gf*8)
-        #u4 = deconv2d(d4, d3, self.gf*8)
-        u5 = deconv2d(d3, d2, self.gf*4)
-        u6 = deconv2d(u5, d1, self.gf*2)
-        u7 = UpSampling2D(size=2)(u6)
-        output_img = Conv2D(self.channels, kernel_size=4, strides=1, padding='same', activation='tanh')(u7)
-        return Model(d0, output_img)
+
+        inp = Input(shape=self.img_shape)
+        data = self.encoder(inp)
+        output_img = self.decoder(data)
+
+        return Model(inp, output_img)
+
     def build_discriminator(self):
-        def d_layer(layer_input, filters, f_size=5, bn=True):
+        def d_layer(layer_input, filters, f_size=4, bn=True):
             """Discriminator layer"""
             d = Conv2D(filters, kernel_size=f_size, strides=2, padding='same')(layer_input)
             d = LeakyReLU(alpha=0.2)(d)
@@ -133,7 +113,7 @@ class Pix2Pix():
                 # ---------------------
                 # Condition on B and generate a translated version
                 fake_A = self.generator.predict(imgs_B)
-                if batch_i % 400 == 0:
+                if batch_i % 200 == 0:
                     for im in range(len(fake_A)):
                         gen_output_img = (fake_A[im] + 1) * 127.5
                         org_data = cv2.imread(imgpaths[im])
@@ -142,7 +122,7 @@ class Pix2Pix():
                         org_data_right = org_data[:, self.img_cols:,:]
                         combined_out_img = np.concatenate((org_data_left,org_data_right, gen_output_img), 1)
                         print(imgpaths[im])
-                        cv2.imwrite("/home/nasheath_ahmed/X-RayShadowRemovalAndClassification/generated_images_3/" + str(epoch) + "_" + str(batch_i) + "_" + imgpaths[im].split("/")[-1], combined_out_img)
+                        cv2.imwrite("/home/nasheath_ahmed/X-RayShadowRemovalAndClassification/generated_images_4/" + str(epoch) + "_" + str(batch_i) + "_" + imgpaths[im].split("/")[-1], combined_out_img)
                         break
                 # Train the discriminators (original images = real / generated = Fake)
                 d_loss_real = self.discriminator.train_on_batch([imgs_A, imgs_B], valid)
@@ -155,7 +135,7 @@ class Pix2Pix():
                 g_loss = self.combined.train_on_batch([imgs_A, imgs_B], [valid, imgs_A])
                 elapsed_time = datetime.datetime.now() - start_time
                 # Plot the progress
-                with open("training3_nov26th.txt", "a") as myfile:
+                with open("training1.txt", "a") as myfile:
                     myfile.write("[Epoch %d/%d] [Batch %d/%d] [D loss: %f, acc: %3d%%] [G loss: %f] time: %s \n" % (epoch, epochs,
                                                                         batch_i, self.data_loader.n_batches,
                                                                         d_loss[0], 100*d_loss[1],
@@ -189,12 +169,48 @@ class Pix2Pix():
         fig.savefig("sampled_images/%s/%d_%d.png" % (self.dataset_name, epoch, batch_i))
         plt.close()
 
+class Encoder(tf.keras.layers.Layer):
+    def __init__(self):
+       super(Encoder, self).__init__()
+       self.encoder_conv_1 = tf.keras.layers.Conv2D(16, 5, strides=(1,1), padding='SAME', activation='relu', kernel_initializer=tf.initializers.random_normal(stddev=0.1))
+       self.encoder_conv_2 = tf.keras.layers.Conv2D(32, 5, strides=(1,1), padding='SAME', activation='relu', kernel_initializer=tf.initializers.random_normal(stddev=0.1))
+       self.encoder_conv_3 = tf.keras.layers.Conv2D(64, 5, strides=(1,1), padding='SAME', activation='relu', kernel_initializer=tf.initializers.random_normal(stddev=0.1))
+    
+    @tf.function
+    def call(self, images):
+        data = self.encoder_conv_1(images)
+        data = tf.nn.max_pool(data, [1,2,2,1], [1,2,2,1], padding='SAME')
+        data = self.encoder_conv_2(data)
+        data = tf.nn.max_pool(data, [1,2,2,1], [1,2,2,1], padding='SAME')
+        data = self.encoder_conv_3(data)
+        data = tf.nn.max_pool(data, [1,2,2,1], [1,2,2,1], padding='SAME')
+        return data
+
+class Decoder(tf.keras.layers.Layer):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        
+        self.decoder_deconv_1 = tf.keras.layers.Conv2DTranspose(32, 5, strides=(2,2), 
+            padding='SAME', activation='relu', kernel_initializer=tf.initializers.random_normal(stddev=0.02))
+        self.decoder_deconv_2 = tf.keras.layers.Conv2DTranspose(16, 5, strides=(2,2), 
+            padding='SAME', activation='relu', kernel_initializer=tf.initializers.random_normal(stddev=0.02))
+        self.decoder_deconv_3 = tf.keras.layers.Conv2DTranspose(3, 5, strides=(2,2), 
+            padding='SAME', activation='tanh', kernel_initializer=tf.initializers.random_normal(stddev=0.1))
+
+    @tf.function
+    def call(self, encoder_output):
+        
+        data = self.decoder_deconv_1(encoder_output)
+        data = self.decoder_deconv_2(data)
+        data = self.decoder_deconv_3(data)
+        return data
+
 if __name__ == '__main__':
     gan = Pix2Pix()
-    gan.train(epochs=25, batch_size=4, sample_interval=200)
-    gan.combined.save_weights("ganWeights.h5")
-    gan.generator.save_weights("generatorWeights.h5")
-    gan.discriminator.save_weights("discriminatorWeights.h5")
+    gan.train(epochs=50, batch_size=4, sample_interval=200)
+    gan.combined.save_weights("weights_run_4/ganWeights.h5")
+    gan.generator.save_weights("weights_run_4/generatorWeights.h5")
+    gan.discriminator.save_weights("weights_run_4/discriminatorWeights.h5")
     receiver_emails = ['hossam_zaki@brown.edu', 'mohamad_abouelafia@brown.edu', 'nasheath_ahmed@brown.edu', 'andrew_aoun@brown.edu']
     for bozo in receiver_emails:
         port = 465
